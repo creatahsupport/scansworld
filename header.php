7d2eb07a4972dec292de8f81f563ff3169325127
allvincreatah <allvin.r@creatah.com>
2025-11-25 17:42:04 +0530

    add the gtm code  in header and body
    add the gtm code  in header and body

--- header.php
+++ header.php
@@ -181,3 +181,7 @@
 
   gtag('config', 'G-CBVPKKHYKX');
 </script>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VV93XFP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
