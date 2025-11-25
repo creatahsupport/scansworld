1aab32df2b83dc689a2e204b700e378048792608
allvincreatah <allvin.r@creatah.com>
2025-11-25 10:28:24 +0530

    add the ga4 tag in head and body
    add the ga4 tag in head and body

--- header.php
+++ header.php
@@ -172,3 +172,12 @@
     </div>
   </div>
 </header>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CBVPKKHYKX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CBVPKKHYKX');
+</script>
--- seo.php
+++ seo.php
@@ -140,3 +140,12 @@ foreach ($result as $row) {
 <?= $schema ?>
 </script>
 <?php endif; ?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CBVPKKHYKX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CBVPKKHYKX');
+</script>
