6cfe8ba444de6a9aeadbeb466859d4d8159c3c3f
venkadesh <venkadesh.n@creatah.com>
2026-01-06 12:51:35 +0530

    Favicon update

--- index.php
+++ index.php
@@ -25,20 +25,20 @@ $latest_doctors = mysqli_query($con, $doctors_query);
   <?php include_once("seo.php");?>
 
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicons/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicons/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicons/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicons/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicons/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicons/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-icon-180x180.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicons/favicon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicons/favicon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicons/favicon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicons/favicon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicons/favicon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/favicon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicons/favicon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/favicon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/favicon-180x180.png">
   <link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicons/android-icon-192x192.png">
   <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicons/favicon-96x96.png">
   <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
-  <link rel="manifest" href="assets/img/favicons/manifest.json">
+  
   <meta name="msapplication-TileColor" content="#ffffff">
   <meta name="msapplication-TileImage" content="assets/img/favicons/ms-icon-144x144.png">
   <meta name="theme-color" content="#ffffff">
